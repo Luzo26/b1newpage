130d6c8b686914b92dd0bd08cff9b1d034a98898
Moises <moisalas@gmail.com>
2022-07-04 22:00:07 +0200

    ajuste final

--- buno/settings.py
+++ buno/settings.py
@@ -34,7 +34,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
